45e9747f695befd622ce68f42d419abfde6a61af
priyanka <pri472575@gmail.com>
2023-08-14 01:24:17 -0700

    nse project

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {NgToastService} from 'ng-angular-popup';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+
+}
